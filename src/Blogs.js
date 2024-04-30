b9bd1c9f86f0887cb9f365637c90aef48d6cca1e
prasanpro <prasanndutt@gmail.com>
2024-04-30 11:44:03 +0530

    Earth Day 24

--- src/Blogs.js
+++ src/Blogs.js
@@ -14,19 +14,20 @@ export const Blogslist = [
     },
     {
         id:2,
-        title : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, ipsam?",
-        image : "logo.png",
-        date : "dd/mm/yyyy",
-        link : "https://www.instagram.com/",
-        description : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eligendi accusamus nihil placeat 
-        nobis expedita ullam cum provident sapiente rerum dolorum repudiandae tenetur atque, a saepe deserunt exercitationem 
-        tempore porro fugit itaque consectetur sit voluptate culpa. Adipisci fuga deleniti, consequuntur labore natus voluptatem 
-        nobis quam cupiditate maxime illum? Quam vero totam veritatis sequi omnis quas repudiandae saepe commodi, delectus iusto 
-        deserunt reiciendis vitae incidunt mollitia  veniam non nemo quasi, expedita, aliquid vel natus dolores minima architecto 
-        ipsam. Minima magni maiores maxime impedit,  velit quasi quia dolorem totam vel molestiae! Temporibus, repudiandae vero? 
-        Delectus doloribus nam illum cum a, libero expedita  incidunt quo hic blanditiis assumenda numquam voluptate molestias quae deleniti pariatur eius accusamus minima quibusdam mollitia   commodi quaerat est! Voluptatum exercitationem beatae dolor. Eius blanditiis quod laboriosam, similique odio tempora aliquid itaque   obcaecati et non alias numquam ex reprehenderit voluptatibus veritatis expedita voluptates voluptatem libero dolorem nihil! Exercitationem   accusamus adipisci vitae quod quasi hic asperiores quas atque illum? Odit amet, sunt eligendi, quo possimus nisi voluptate laboriosam sint explicabo   dolorem deserunt aliquam adipisci. Saepe ducimus dicta dolores excepturi 
-        recusandae blanditiis, natus autem? Fugit, explicabo pariatur. Et ex quidem vel,beatae veniam fuga corporis labore! 
-        Impedit, ipsam! Harum obcaecati repudiandae minima?`
+        title : "Earth Day 2024",
+        image : "R30 Facelift.jpg",
+        date : "30/04/2024",
+        link : "https://www.instagram.com/reel/C6T-t6ayMrE/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        description : `Today is end of April 2024. This month has been a rocky ride for Rango E-Cycles. We have done some amazing thing throughout this month. At the beginning of this month, EMotorad special meeting was held in which MSD was officially announced as investor and brand ambassador. The news was really special as entry of someone who is ideal for millions is something which is really needed for electric bicycle segment. We got the information that there will be a special edition named Legend 07 will be released for Dhoni’s contribution in India cricket. This new gave us a morale boost and we got goosebump after seeing first theme cum teaser video made on Dhoni aka Legend 07.
+        https://www.instagram.com/reel/C5VepGlSwKc/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==
+        Following week, we got a big surprise once Dhoni seen as singing a piece of Phalguni Pathak’s famous song – “Yaad Piya Ki Aane Lagi” in an ad video. The surprise was so big that this “DHONI” became top 2nd trending tag on twitter and there were many beautiful comments received on the ad. Some top choice comments are – “Maahi Gaa Raha Hai”, “Ye marketing walo ne kya kara diya”. Another surprise came of weekend where “Bole Jo Koyal” was played by DJ in IPL match between CSK & KKR. During this match, this ad was played several times and I was just thinking in back of my mind, how the trend of electric bicycle is reaching 12Cr+ Indian households. I have never seen such trend in electric bicycle industry since last 7 years. This not even happened during Salman Khan’s Being Human electric bicycle launch. We started marketing ourself as – “Dhoni ka haath jis team pe hoga wo team hamesa upar hi jayegi”.
+        https://www.instagram.com/reel/C5fNxyKS_0E/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==
+        During 2nd week of April, we also started our new store in Arrah. This was a long-term thinking but as being said, you just start with full enthusiasm and you’re already half done. The greatest challenge to a toughest job is just getting started. We put some electric bicycles on display and made our google maps page [link] live. This store was started with intention to reach people from all places near Arrah, Chhapra, Dehri, Sasaram, Aurangabad, Jagdishpur, Bikramganj, Buxar, Bihiya etc. For now, the place is ready as an additional location of Rango E-Cycles but more refinement will be done in future to make it a customer friendly place.
+        3rd week of April was planned for RideAsia 2024 B2B event organized in Pragati Maidan, New Delhi. Summer started picking up and in this screeching heat, I reached Delhi to visit the event. Just as I was planning my day and busy locating Supreme Court metro station on the route chart, I was thinking I might have crossed this metro station several times but never thought nor got a chance to come out of station to visit the place. In order to explore and get a feel of Pragati Maidan, I decided to walk to the exhibition hall. The place is so huge that it took approx. 20 min for me to reach exhibition hall #2. RideAsia event gave me a completely different perspective of bicycle industry. It was larger event than CFOS Ludhiana and having different manufacturers and suppliers from across the India. It also has European market exposure as few Indian companies (such as Navyug Namdari’s Verdant) displayed the cycles which is designed and priced to sell in European market only. Met few innvoators who were doing great on the solving common pain point faced by electric bicycle users such as PedalX. It was also surprising to know that Reliance also having a range of electric cycles under the brand-named Astra. Some glimpse from the event is available on our official youtube https://youtu.be/Y2JqUr3gBuo?si=vxS2HwsgM_YQCAIh.
+        Coming to last week of April, the most audacious task has been done. Since the inception of Rango E-Cycles, we were planning to launch our own brand electric cycle. Getting support from different suppliers and manufacturers, we assembled our first electric cycles and named it R30. With 30KM range, this cycle is having 5 pedal assist mode, LCD display with odometer and other basic information. This cycle is feature loaded and comes at a competitive price to other branded cycles with similar specifications. More detailed teaser video and product launch is planned in May. Stay Tuned and Happy Cycling 😊
+        
+        Happy Earth Day 2024 😊
+        `
     },
     {
         id:3,
